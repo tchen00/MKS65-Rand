591cf9e8b212c84dba3443fbc2627ccdc3025ad3
Tammy Chen <tchen00@stuy.edu>
2019-11-08 20:19:36 -0500

    finished & updated

--- main.c
+++ main.c
@@ -1,41 +1,61 @@
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
-# include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
 
-int * gen_rand(){
-  int rand_file = open("/dev/random", O_RDONLY);
+int rand_gen(){
+  int file = open("/dev/random", O_RDONLY);
   int i;
-  int random_int = read(rand_file, &i, sizeof(int));
-  close(rand_file);
+  read(file, &i, sizeof(int));
+
+  if (errno) { // hopefully not
+    printf("Errno number: %d\n", errno);
+    printf("Message: %s\n", strerror(errno));
+  }
+
+  close(file);
   return i;
 }
 
+
 int main(){
-  int arr[10];
+  int rand[10];
 
   printf("Generating random numbers:\n");
   int i;
-  for(i = 0; i < 10; i++) {
-    arr[i] = gen_rand();
-    printf("\trandom %d: %d\n", i, arr[i]);
+  for (i = 0; i < 10; i++){
+    rand[i] = rand_gen();
+    printf("\trandom %d: %d\n", i, rand[i]);
   }
 
-  printf("Writing numbers to file...\n");
-  int file = open("text.txt", O_WRONLY);
-  write(file, arr, 10 * sizeof(int));
-  close(file);
+  printf("\nWriting numbers to file...\n");
 
-  printf("Reading numbers from file...\n");
-  file = open("text.txt", O_RDONLY);
-  int arr2[10];
-  read(file, arr2, 10 * sizeof(int));
-  close(file);
+  int file2 = open("random.txt", O_RDWR|O_CREAT, 0666);
+  write(file2, rand, sizeof(rand));
+  close(file2);
 
-  printf("Verification that written values were the same:\n");
-  for(i = 0; i < 10; i++) {
-    printf("\trandom %d: %d\n", i, arr2[i]);
+  if (errno) { //DEBUGGING -- hopefully not
+        printf("Errno number: %d\n", errno);
+        printf("Message: %s\n", strerror(errno));
+  }
+
+  printf("\nReading numbers from file...\n");
+  int new[10];
+  int file3 = open("random.txt", O_RDONLY);
+  read(file3, &new, sizeof(new));
+  close(file3);
+
+  if (errno) {
+        printf("Errno number: %d\n", errno);
+        printf("Message: %s\n", strerror(errno));
+  }
+
+  printf("\nVerification that written values were the same:\n");
+  for ( i = 0; i < 10; i++){
+    printf("\trandom %d: %d\n", i, new[i]);
   }
 
   return 0;
+
 }
